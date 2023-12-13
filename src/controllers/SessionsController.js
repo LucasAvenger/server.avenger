d2de4895c0c5a3a97181dbaecf82523518ece69f
lucas_simoes <lukas.sps19@gmail.com>
2023-12-13 00:03:04 -0300

    novo

---
+++ src/controllers/SessionsController.js
@@ -0,0 +1,15 @@
+const knex = require("../database/knex");
+const AppError = require("../utils/AppError")
+
+class SessionsController {
+  async create(request, response) {
+    const { email, password } = request.body;
+
+    const user = await knex("users").where({ email }).first();
+
+
+    return response.json({ email, password });
+
+  }
+}
+module.exports = SessionsController;
